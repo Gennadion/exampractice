d9674937a7a1dae72f2543399a9a97271bba5b78
Gennadion <ghammurti@gmail.com>
2021-05-27 20:55:10 +0300

    Created source file, main function and function prototypes

--- exampractice/exampractice.cpp
+++ exampractice/exampractice.cpp
@@ -30,49 +30,6 @@ int main()
     
 }
 
-/**
- * Function <code>showMenu</code> displays the menu that is used from
- * the main program
- * <BR>
- */
-void showMenu()
-{
 
-}
-
-/**
- * Function <code>sumOfRange</code> adds all elements in a defined range.
- * <BR>
- * @param startVal Starting index of the value from which addition will start.
- * @param endVal Last index of the value to be added from the range.
-  * @return Returns the sum.
- */
-int sumOfRange(int startVal, int endVal)
-{
-
-}
-
-/**
- * Function <code>productOfRange</code> computes the product of the elements in a defined range.
- * <BR>
- * @param startVal Starting index of the value from which multiplication will start.
- * @param endVal Last index of the value to be multiplied from the range.
- * @return Returns the result of the product.
- */
-int productOfRange(int startVal, int endVal)
-{
-
-}
-
-/**
- * Function <code>productOfRange</code> computes the product of the elements in a defined range.
- * <BR>
- * @param value A number to be determined if positive or negative
- * @return Returns <code>true</code> if the number is positive, <code>false</code> if the number is negative.
- */
-bool isPositive(double value)
-{
-
-}
 
 
