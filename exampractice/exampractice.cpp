a1d5f2f1f6f3fd6c24dd9d199ea5b40aad25d273
Gennadion <ghammurti@gmail.com>
2021-05-27 20:51:30 +0300

    Add project files.

---
+++ exampractice/exampractice.cpp
@@ -0,0 +1,78 @@
+/** \file exampractice.cpp
+ *  \brief     A little program to illustrate commenting of C++ source files.
+ *  \details   The program illustrates a dynamic array implementation
+ *  \author    Gennady Petrishchev
+ *  \version   0.1
+ *  \date      2020-2021
+ *  \pre       First initialize the system.
+ *  \bug       No bugs so far
+ *  \copyright University of Nicosia.
+ */
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+//Function Prototypes
+int sumOfRange(int startVal, int endVal);
+int productOfRange(int startVal, int endVal);
+bool isPositive(double value);
+void showMenu();
+
+/**
+ * The <code>main</code> is the driver function that kick-starts the
+ * application.
+ * <BR>
+ * @return Returns <code>0</code> on success, any other value otherwise.
+ */
+int main()
+{
+    
+}
+
+/**
+ * Function <code>showMenu</code> displays the menu that is used from
+ * the main program
+ * <BR>
+ */
+void showMenu()
+{
+
+}
+
+/**
+ * Function <code>sumOfRange</code> adds all elements in a defined range.
+ * <BR>
+ * @param startVal Starting index of the value from which addition will start.
+ * @param endVal Last index of the value to be added from the range.
+  * @return Returns the sum.
+ */
+int sumOfRange(int startVal, int endVal)
+{
+
+}
+
+/**
+ * Function <code>productOfRange</code> computes the product of the elements in a defined range.
+ * <BR>
+ * @param startVal Starting index of the value from which multiplication will start.
+ * @param endVal Last index of the value to be multiplied from the range.
+ * @return Returns the result of the product.
+ */
+int productOfRange(int startVal, int endVal)
+{
+
+}
+
+/**
+ * Function <code>productOfRange</code> computes the product of the elements in a defined range.
+ * <BR>
+ * @param value A number to be determined if positive or negative
+ * @return Returns <code>true</code> if the number is positive, <code>false</code> if the number is negative.
+ */
+bool isPositive(double value)
+{
+
+}
+
+
